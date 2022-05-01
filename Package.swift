eaf0c0587eee7538893fd32646f633c9252f03b7
tsurumoto <tsurumotokentarou@gmail.com>
2022-05-01 17:54:56 +0900

    fix exclude

--- Package.swift
+++ Package.swift
@@ -20,10 +20,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "RangeSlider",
-            dependencies: []),
+            dependencies: [],
+            exclude: ["Example"]),
         .testTarget(
             name: "RangeSliderTests",
             dependencies: ["RangeSlider"]),
-        .exclude: ["Example"]
     ]
 )
